21c23cfb605b15cd64d8ad2900e6027d0967bc35
mushcatshiro <yingjieho@hotmail.com>
2023-02-18 16:52:03 +0800

    removing rq connection hardcoding

--- Software Engineering/examples/mq_dist/README.md
+++ Software Engineering/examples/mq_dist/README.md
@@ -14,7 +14,7 @@ as database.
 - [ ] separation of client and app
   - i.e. current commit client and app shares same project folder
   - workaround for namespace search
-- [ ] remove rq connection hardcoding
+- [x] remove rq connection hardcoding
 - [ ] endpoint for job checking
 
 ## setup
--- Software Engineering/examples/mq_dist/app/client.py
+++ Software Engineering/examples/mq_dist/app/client.py
@@ -1,12 +1,13 @@
 import os
 
-import redis
-from rq import Worker, Queue, Connection
+# import redis
+from redis import Redis
+from rq import Worker, Queue, Connection, use_connection
 
 
 if __name__ == "__main__":
     name = os.environ.get("CLIENTNAME")
-    redis_connection = redis.from_url("redis://redis:6379/0")
-    with Connection(redis_connection):
+    # redis_connection = redis.from_url("redis://redis:6379/0")
+    with Connection(Redis(host="redis", port=6379, db=0)):
         queue = Queue()
         Worker(queue, name=f"worker{name}").work()
